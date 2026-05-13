376b24cc7eef59c2b215adcabeb8df1556ea85ca
Maksym-UA <max.savin3@gmail.com>
2026-05-14 00:19:19 +0300

    Init the project

---
+++ lib/mqtt/mqtt.h
@@ -0,0 +1,17 @@
+#ifndef MQTT_H
+#define MQTT_H
+
+#include "mqtt_client.h"
+
+#define MQTT_BROKER_URI  "mqtt://broker.hivemq.com:1883"
+#define MQTT_TOPIC       "esp32s3/test"
+#define MQTT_COMMANDS    "esp32s3/commands"
+#define MQTT_STATUS      "esp32s3/status"
+
+typedef void (*mqtt_message_handler_t)(const char *topic, const char *data);
+
+void mqtt_app_start(void);
+esp_mqtt_client_handle_t mqtt_get_client(void);
+void mqtt_set_message_handler(mqtt_message_handler_t handler);
+
+#endif // MQTT_H
---
+++ lib/wifi/wifi_setup.h
@@ -0,0 +1,7 @@
+#ifndef WIFI_SETUP_H
+#define WIFI_SETUP_H
+
+// Ініціалізує Wi-Fi в режимі клієнта (STA)
+void wifi_init_sta(void);
+
+#endif // WIFI_SETUP_H
